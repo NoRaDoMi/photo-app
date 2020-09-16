269617f1091c52d53d389bb011433b09da1338f8
Noradomi <fakemsi1999@gmail.com>
2020-09-16 23:40:46 +0700

    setup project

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+Header.propTypes = {
+    
+};
+
+function Header(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Header;
