0b6da88a929ceed9d34c03ec3bdb66ebe2bd68a0
Noradomi <fakemsi1999@gmail.com>
2020-09-17 14:14:56 +0700

    setup reactstrap ui

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
 
-Header.propTypes = {
-    
-};
+Header.propTypes = {};
 
 function Header(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<header className="header">
+			<Container>
+				<Row className="justify-content-between">
+					<Col xs="auto">
+						<a
+							href="http://google.com.vn"
+							className="header__link header__title"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Noradomi
+						</a>
+					</Col>
+
+					<Col xs="auto">
+						<NavLink exact className="header__link" to="/photos" activeClassName="header__link--active">
+							React - Redux Project
+						</NavLink>
+					</Col>
+				</Row>
+			</Container>
+		</header>
+	);
 }
 
 export default Header;
